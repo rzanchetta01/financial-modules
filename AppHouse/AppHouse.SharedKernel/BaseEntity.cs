86edf111b8ae9b38f7d9000f34b2cf2427c9ee45
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2024-01-30 00:39:55 -0300

    SharedKernel initial implementation

---
+++ AppHouse/AppHouse.SharedKernel/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppHouse.SharedKernel
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
