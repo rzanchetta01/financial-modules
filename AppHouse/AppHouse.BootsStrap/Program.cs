2da47bf444458f47757b1724e49b9640fae41db6
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2024-02-01 19:10:12 -0300

    Add DbContext pooling

--- AppHouse/AppHouse.BootsStrap/Program.cs
+++ AppHouse/AppHouse.BootsStrap/Program.cs
@@ -1,10 +1,17 @@
 using AppHouse.Accounts.Core;
 using AppHouse.BootsStrap.Endpoints;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services DI
 AppHouse.Accounts.Application.StartupServices.AddAccountStartup(builder.Services);
-builder.AddNpgsqlDbContext<AccountsContext>("dbPostgres", e => e.ConnectionString = builder.Configuration.GetConnectionString("account"));
+builder.AddNpgsqlDbContext<AccountsContext>("dbPostgres", e => 
+{
+    e.ConnectionString = builder.Configuration.GetConnectionString("account");
+    e.DbContextPooling = true;
+});
+
+builder.Services.AddMemoryCache();//TODO change later to redis
 
 builder.AddServiceDefaults();
 builder.Services.AddMediatR(c => 
