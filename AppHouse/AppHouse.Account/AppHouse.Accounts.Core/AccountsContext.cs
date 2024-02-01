503c51e239738d572ed782571f041e6804d47904
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2024-02-01 18:47:17 -0300

    Db First Context

--- AppHouse/AppHouse.Account/AppHouse.Accounts.Core/AccountsContext.cs
+++ AppHouse/AppHouse.Account/AppHouse.Accounts.Core/AccountsContext.cs
@@ -1,25 +1,62 @@
 ﻿using AppHouse.Accounts.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
 
-namespace AppHouse.Accounts.Core
+namespace AppHouse.Accounts.Core;
+
+public partial class AccountsContext(DbContextOptions<AccountsContext> options) : DbContext(options)
 {
-    public class AccountsContext(DbContextOptions<AccountsContext> options) : DbContext(options)
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public virtual DbSet<Account> Accounts { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        modelBuilder.Entity<Account>(entity =>
         {
-            base.OnConfiguring(optionsBuilder);
-        }
+            entity.HasKey(e => e.Id).HasName("account_pkey");
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Account>(e =>
-            {
-                e.ToTable("account");
-            });
+            entity.ToTable("account");
 
-            //base.OnModelCreating(modelBuilder);
-        }
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Address).HasColumnName("address");
+            entity.Property(e => e.AddressComplement).HasColumnName("address_complement");
+            entity.Property(e => e.BirthDate).HasColumnName("birth_date");
+            entity.Property(e => e.Cellphone)
+                .HasMaxLength(20)
+                .HasColumnName("cellphone");
+            entity.Property(e => e.City)
+                .HasMaxLength(100)
+                .HasColumnName("city");
+            entity.Property(e => e.Country)
+                .HasMaxLength(100)
+                .HasColumnName("country");
+            entity.Property(e => e.CreditScore).HasColumnName("credit_score");
+            entity.Property(e => e.DateCreated)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnName("date_created");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .HasColumnName("email");
+            entity.Property(e => e.Income)
+                .HasPrecision(18, 2)
+                .HasColumnName("income");
+            entity.Property(e => e.IsActive).HasColumnName("is_active");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .HasColumnName("password");
+            entity.Property(e => e.PostalCode)
+                .HasMaxLength(20)
+                .HasColumnName("postal_code");
+            entity.Property(e => e.State)
+                .HasMaxLength(100)
+                .HasColumnName("state");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
     }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
