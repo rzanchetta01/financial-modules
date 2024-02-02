c45c0b9ab0df7f9876513ac960fd4b817bef850f
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2024-02-02 01:09:27 -0300

    AccountDetails implementation

---
+++ AppHouse/AppHouse.Account/AppHouse.Accounts.Application/Handlers/Queries/GetAccountDetailsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using AppHouse.Accounts.Application.Requests.Queries;
+using AppHouse.Accounts.Core.Interfaces;
+using AppHouse.Accounts.Domain.Dto;
+using MediatR;
+
+namespace AppHouse.Accounts.Application.Handlers.Queries
+{
+    public class GetAccountDetailsQueryHandler(IAccountService service) : IRequestHandler<GetAccountDetailsQueryRequest, AccountDto?>
+    {
+        private IAccountService _service = service;
+        public async Task<AccountDto?> Handle(GetAccountDetailsQueryRequest request, CancellationToken cancellationToken)
+        {
+            return await _service.FindById(request.AccountId, cancellationToken);
+        }
+    }
+}
---
+++ AppHouse/AppHouse.Account/AppHouse.Accounts.Application/Requests/Queries/GetAccountDetailsQueryRequest.cs
@@ -0,0 +1,7 @@
+﻿using AppHouse.Accounts.Domain.Dto;
+using MediatR;
+
+namespace AppHouse.Accounts.Application.Requests.Queries
+{
+    public record GetAccountDetailsQueryRequest(Guid AccountId) : IRequest<AccountDto?>;
+}
