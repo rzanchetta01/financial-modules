f1b1c266c25fe4793738ad2c45a873d30864e352
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2024-02-04 22:30:41 -0300

    update tables scripts

--- AppHouse/AppHouse.DbScripts/AccountActivityHistoryTable.sql
+++ AppHouse/AppHouse.DbScripts/AccountActivityHistoryTable.sql
@@ -2,7 +2,7 @@
 	id UUID PRIMARY KEY,
     date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     is_active BOOLEAN DEFAULT true,
-    account_id UUID NOT NULL REFERENCES accounts(id),
-    loan_id UUID NOT NULL REFERENCES loans(id),
+    account_id UUID NOT NULL REFERENCES account(id),
+    loan_id UUID NOT NULL REFERENCES loan(id),
     was_paid_correctly BOOLEAN NOT NULL
 );
--- AppHouse/AppHouse.DbScripts/LoanTable.sql
+++ AppHouse/AppHouse.DbScripts/LoanTable.sql
@@ -1,8 +1,8 @@
-﻿CREATE TABLE IF NOT EXISTS loans (
+﻿CREATE TABLE IF NOT EXISTS loan (
 	id UUID PRIMARY KEY,
     date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     is_active BOOLEAN DEFAULT true,
-    creator_account_id UUID NOT NULL REFERENCES accounts(id),
+    creator_account_id UUID NOT NULL REFERENCES account(id),
     max_Amount DECIMAL NOT NULL,
     min_Amount DECIMAL NOT NULL,
     max_date_feasible DATE NOT NULL,
