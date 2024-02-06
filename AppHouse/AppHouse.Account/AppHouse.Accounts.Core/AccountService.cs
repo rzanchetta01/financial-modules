c18ac69c5455f3691e51c4dc0eb0286e9adbc314
Henry Goes Zanin <henrygzanin@gmail.com>
2024-02-06 16:20:31 -0300

    feat - last name check and role call
    added first name check with last name and function call when creating a new account

--- AppHouse/AppHouse.Account/AppHouse.Accounts.Core/AccountService.cs
+++ AppHouse/AppHouse.Account/AppHouse.Accounts.Core/AccountService.cs
@@ -14,21 +14,26 @@ namespace AppHouse.Accounts.Core
 
         public async Task Create(AccountDto dto, CancellationToken token)
         {
+
+            int startAccountRating = DefineStartAccountRating(dto, token);
+            dto.CreditScore = startAccountRating;
+
             await _accountRepository.CreateAsync(AccountMapping.Map(dto), token);
         }
 
-        public Task<int> DefineStartAccountRating(AccountDto account, CancellationToken token)
+        public int DefineStartAccountRating(AccountDto account, CancellationToken token)
         {
-            int CreditScore = 0
-
+            int CreditScore = 0;
             if account.Balance >= 6000
                 CreditScore += 1;
             if account.Age >= 27
                 CreditScore += 1;
             if account.AddressComplement is not null
                 CreditScore += 2;
+            if account.Name.Contains(" ") // has a surname or is included in the rule of compound names
+                CreditScore += 1;
 
-            return Task.FromResult(CreditScore);
+            return FromResult(CreditScore);
         }
 
         public async Task<AccountDto?> FindById(Guid Id, CancellationToken token)
@@ -36,7 +41,6 @@ namespace AppHouse.Accounts.Core
             var entity = await _accountRepository.FindByIdAsync(Id, token);
             if(entity is not null)
                 return AccountMapping.Map(entity);
-
             return null;
         }
 
