f1e2cb3247a9d338a17f58fc60e66a1642f4b2f4
Henry Goes Zanin <henrygzanin@gmail.com>
2024-02-09 18:00:28 -0300

    feat - Define start credit score (#3)
    * feat - Define start credit score
    
    * fix - value of the sum
    
    if the complement is not null
    
    * feat - last name check and role call
    
    added first name check with last name and function call when creating a new account
    
    * fix - birthdate and IAccountInterface
    
    I was using Age which does not exist in the account class and the interface was still asynchronous
    
    * Sincronize DateTImes
    
    * SoftDelete impl,  applicaiton should work only with active entities
    
    * Account validators and minor fixes
    
    * Fix wrong method call
    
    * account tests
    
    * minor fix in request
    
    * Add debug infos
    
    * remove useless using
    
    * Add collection mapping
    
    * Account repository tests with in memory database
    
    * Update EventLoggingAndValidationMiddleware call
    
    * Protect entity agaist mistakes
    
    * Remove obsolete domain projects since it moved to shared kernel
    mapping should stay in their layers
    
    * misc changes
    
    * fix #2 - removed tokens and DefineStartAccountRating fix
    
    * feat #2 - PassDefineStartAccountRating test
    
    * fix #2 - CreateaccountValidator
    
    ---------
    
    Co-authored-by: Rodrigo Zanchetta <rzanchetta01@gmail.com>

--- AppHouse/AppHouse.Account/AppHouse.Accounts.Core/AccountService.cs
+++ AppHouse/AppHouse.Account/AppHouse.Accounts.Core/AccountService.cs
@@ -1,27 +1,56 @@
 ﻿using AppHouse.Accounts.Core.Interfaces;
 using AppHouse.SharedKernel.DTOs;
 
+
 namespace AppHouse.Accounts.Core
 {
+
     public class AccountService
         (
         IAccountRepository accountRepository
         )
         : IAccountService
     {
+
         private readonly IAccountRepository _accountRepository = accountRepository;
+
 
         public async Task Create(AccountDto dto, CancellationToken token)
         {
-            await _accountRepository.CreateAsync(AccountMapping.Map(dto), token);
+
+            int startAccountRating = DefineStartAccountRating(dto);
+
+            var newDto = dto with { CreditScore = startAccountRating };
+            await _accountRepository.CreateAsync(AccountMapping.Map(newDto), token);
         }
 
-        public Task<int> DefineStartAccountRating(AccountDto account, CancellationToken token)
+        public int DefineStartAccountRating(AccountDto account)
         {
-            if (account.Name.StartsWith("y", StringComparison.OrdinalIgnoreCase))
-                return Task.FromResult(5);
+            int CreditScore = 0;
+            if (account.Income >= 6000)
+                CreditScore += 1;
 
-            return Task.FromResult(0);
+            DateTime birthDate;
+            if (DateTime.TryParse(account.BirthDate, out birthDate))
+            {
+                DateTime currentDate = DateTime.Today;
+                int age = currentDate.Year - birthDate.Year;
+
+                if (birthDate.Date > currentDate.AddYears(-age))
+                {
+                    age--;
+                }
+                if (age >= 27)
+                    CreditScore += 1;
+            }
+
+            if (account.AddressComplement is not null)
+                CreditScore += 2;
+
+            if (account.Name.Trim().Contains(" ")) // if name has " " it means it has more than one name
+                CreditScore += 1;
+
+            return CreditScore;
         }
 
         public async Task<AccountDto?> FindById(Guid Id, CancellationToken token)
@@ -29,7 +58,6 @@ namespace AppHouse.Accounts.Core
             var entity = await _accountRepository.FindByIdAsync(Id, token);
             if(entity is not null)
                 return AccountMapping.Map(entity);
-
             return null;
         }
 
@@ -43,4 +71,5 @@ namespace AppHouse.Accounts.Core
             await _accountRepository.UpdateAsync(AccountMapping.Map(dto), token);
         }
     }
+
 }
