0c375846d17e62d8ba59aff703f50008fb729c65
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2024-02-15 22:54:30 -0300

    Dispose loan event impl

---
+++ AppHouse/AppHouse.Loans/AppHouse.Loans.Application/Events/DisposeLoanEvent.cs
@@ -0,0 +1,20 @@
+﻿using AppHouse.Loans.Core.Interfaces;
+using AppHouse.SharedKernel.BasicEvents;
+using AppHouse.SharedKernel.DTOs;
+using MediatR;
+
+namespace AppHouse.Loans.Application.Events
+{
+    public class DisposeLoanEvent(ILoanService loanService) : INotificationHandler<TEventPurged<LoanDto>>
+    {
+        private readonly ILoanService _loanService = loanService;
+
+        public async Task Handle(TEventPurged<LoanDto> notification, CancellationToken cancellationToken)
+        {
+            if(notification.data.Id is not null)
+                await _loanService.Purge(notification.data.Id.Value, cancellationToken);
+
+                
+        }
+    }
+}
