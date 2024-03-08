6dd80abd57d45f69f25f3fe1ef36126f0c75442b
Henry Goes Zanin <henrygzanin@gmail.com>
2024-03-08 15:47:52 -0300

    feat - FailCreateRangeAsyncTest

--- AppHouse/AppHouse.Tests/Loans/LoanRepositoryTests.cs
+++ AppHouse/AppHouse.Tests/Loans/LoanRepositoryTests.cs
@@ -30,6 +30,28 @@ namespace AppHouse.Tests.Loans
             Assert.Equal(1, await context.Loans.CountAsync());
 
         }
+
+        [Fact]
+        public async Task FailCreateRangeAsyncTest()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<LoanContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new LoanContext(options);
+
+            var uat = new LoanRepository(context);
+            var data = LoanMapping.Map(DummyData.DummyNewLoanDto);
+            var token = CancellationToken.None;
+
+            //Act
+            await uat.CreateRangeAsync(new List<Loan> { data }, token);
+
+            //Assert
+            await Assert.ThrowsAnyAsync<Exception>(async () => await uat.CreateRangeAsync(new List<Loan> { data }, token));
+            Assert.Equal(1, await context.Loans.CountAsync());
+        }
         
     }
 }
