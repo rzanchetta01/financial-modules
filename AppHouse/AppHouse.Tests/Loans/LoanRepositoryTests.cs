7ad4a1bca6d3871c5fda31a150660449fe236bdb
Henry Goes Zanin <henrygzanin@gmail.com>
2024-03-11 13:23:28 -0300

    #4 testes para operações de empréstimos (#6)
    * feat - Define start credit score
    
    * fix - value of the sum
    
    if the complement is not null
    
    * feat - last name check and role call
    
    added first name check with last name and function call when creating a new account
    
    * fix - birthdate and IAccountInterface
    
    I was using Age which does not exist in the account class and the interface was still asynchronous
    
    * fix #2 - removed tokens and DefineStartAccountRating fix
    
    * feat #2 - PassDefineStartAccountRating test
    
    * fix #2 - CreateaccountValidator
    
    * feat #4 - PassCreateLoanCommandTest
    
    * feat #4 - FailCreateLoanCommandTest
    
    * feat #4 - PassCreateLoanValidatorTest
    
    * feat #4 - FailCreateLoanValidatorTest
    
    * fix - PassCreateLoanServiceTest
    
    concerted test of creating a loan service and renaming CreateLoanCommand
    
    * feat #5 - FailCreateOanServiceTest
    
    * fix #5 - .NET / build (pull_request)
    
    * fix #5
    
    * teste
    
    * test - remove test, loans
    
    * fix - loan tests
    
    * fix - _mediator.publish
    
    the publish method was not being called after creating the loan
    
    * feat - PassCreateRangeAsyncTest
    
    * feat - FailCreateRangeAsyncTest

---
+++ AppHouse/AppHouse.Tests/Loans/LoanRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using AppHouse.Loans.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppHouse.Tests.Loans
+{
+    public class LoanRepositoryTests
+    {
+        [Fact]
+        public async Task PassCreateRangeAsyncTest()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<LoanContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new LoanContext(options);
+
+            var uat = new LoanRepository(context);
+            var data = LoanMapping.Map(DummyData.DummyNewLoanDto);
+            var token = CancellationToken.None;
+
+            //Act
+            await uat.CreateRangeAsync(new List<Loan> { data }, token);
+
+            //Assert
+            Assert.Equal(1, await context.Loans.CountAsync());
+
+        }
+
+        [Fact]
+        public async Task FailCreateRangeAsyncTest()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<LoanContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new LoanContext(options);
+
+            var uat = new LoanRepository(context);
+            var data = LoanMapping.Map(DummyData.DummyNewLoanDto);
+            var token = CancellationToken.None;
+
+            //Act
+            await uat.CreateRangeAsync(new List<Loan> { data }, token);
+
+            //Assert
+            await Assert.ThrowsAnyAsync<Exception>(async () => await uat.CreateRangeAsync(new List<Loan> { data }, token));
+            Assert.Equal(1, await context.Loans.CountAsync());
+        }
+        
+    }
+}
